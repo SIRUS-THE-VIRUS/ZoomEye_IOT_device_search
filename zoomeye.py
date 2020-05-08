d3d8fac07aa5f971bfe0d5be26054b3f7a391bdf
La-Shawn Reynolds <40344706+SIRUS-THE-VIRUS@users.noreply.github.com>
2020-05-08 00:54:01 -0500

    Authentication added

--- zoomeye.py
+++ zoomeye.py
@@ -1,10 +1,15 @@
 import requests
 import urllib.parse
+import getpass
 from tabulate import tabulate
 
-#make it so that you can get the token automatically(input username password and get token
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6ImxvdHNwYWNlNjE5QGdtYWlsLmNvbSIsImlhdCI6MTU4ODkxMDk0NCwibmJmIjoxNTg4OTEwOTQ0LCJleHAiOjE1ODg5NTQxNDR9.dn_02gyeZX9JXyZpifRfu8JbabzfjXuSy7nyO8PDWYU"
-query = "country:Jamaica&page=1"
+username = input("Username: ")
+password = getpass.getpass()
+data = '{{"username": "{}", "password": "{}"}}'.format(username,password)
+token_response = requests.post("https://api.zoomeye.org/user/login",data=data)
+token = token_response.json()['access_token']
+
+url = ""
 host_filters = ["app", 'app:proftd'], ["ver", 'ver:2.1'], ['ip', 'ip:1.1.1.1'], ['service', 'service:http'], \
                ['cidr', '1.2.3.4/21'], ['hostname', 'google.com'], ['port', 'port:53'], ['city', 'city:beijin'], \
                ['country', 'country:china'], ['asn', 'asn:1234'], ['device', 'device:router'], ['os', 'os:windows']
@@ -25,17 +30,10 @@ elif choice == 2:
     encoded_query = urllib.parse.quote_plus(query)
     url = "https://api.zoomeye.org/web/search?query=" + encoded_query + "&page=1"
 
-#input must look like this below
-#country:Jamaica +port:8080
-# query = "country:jamaica +port:8080"
-# encoded_query = urllib.parse.quote_plus(query)
-# print(encoded_query)
-# url = "https://api.zoomeye.org/host/search?query=" + encoded_query + "&page=1"
 print(url)
 response = requests.get(url, headers={"Authorization": "JWT " + token})
 print("\n")
 json_data = response.json()
-#print(json_data)
 length = len(json_data["matches"])
 print(length)
 for i in range(length):
