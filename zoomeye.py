40c24a9cfab23b98af168d3fd443c56fbee01db8
La-Shawn Reynolds <40344706+SIRUS-THE-VIRUS@users.noreply.github.com>
2020-05-07 23:44:44 -0500

    Fixed multiple queries issue
    Added url encoding

--- zoomeye.py
+++ zoomeye.py
@@ -1,8 +1,9 @@
 import requests
+import urllib.parse
 from tabulate import tabulate
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6ImxvdHNwYWNlNjE5QGdtYWlsLmNvbSIsImlhdCI6MTU4ODcxOTk5OCwibmJmIjoxNTg4NzE5OTk4LCJleHAiOjE1ODg3NjMxOTh9.-3AaRuLHrMx2PiyCqlERdjyi2_KUk5eyE20hwtoaLzo"
-
+#make it so that you can get the token automatically(input username password and get token
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6ImxvdHNwYWNlNjE5QGdtYWlsLmNvbSIsImlhdCI6MTU4ODkxMDk0NCwibmJmIjoxNTg4OTEwOTQ0LCJleHAiOjE1ODg5NTQxNDR9.dn_02gyeZX9JXyZpifRfu8JbabzfjXuSy7nyO8PDWYU"
 query = "country:Jamaica&page=1"
 host_filters = ["app", 'app:proftd'], ["ver", 'ver:2.1'], ['ip', 'ip:1.1.1.1'], ['service', 'service:http'], \
                ['cidr', '1.2.3.4/21'], ['hostname', 'google.com'], ['port', 'port:53'], ['city', 'city:beijin'], \
@@ -13,20 +14,28 @@ web_filters = [], [], [], [], [], [], [], []
 choice = int(input("what are you searching?\n1)host 2) web\n=======>>>>> "))
 if choice == 1:
     print(tabulate(host_filters, headers=['Name', 'Description']))
-    print("EXAMPLE: country:jamaica&port:8080")
+    print("EXAMPLE: country:jamaica +port:8080 +service:http")
     query = input("Enter search string: ")
-    url = "https://api.zoomeye.org/host/search?query=" + query + "&page=1"
+    encoded_query = urllib.parse.quote_plus(query)
+    url = "https://api.zoomeye.org/host/search?query=" + encoded_query + "&page=1"
 elif choice == 2:
     print(tabulate(web_filters, headers=['Name', 'Description']))
-    print("EXAMPLE: country:jamaica&port:8080")
+    print("EXAMPLE: country:jamaica +port:8080 +service:http")
     query = input("Enter search string: ")
-    url = "https://api.zoomeye.org/web/search?query=" + query + "&page=1"
+    encoded_query = urllib.parse.quote_plus(query)
+    url = "https://api.zoomeye.org/web/search?query=" + encoded_query + "&page=1"
 
-#currently not able to have multiple queries for example port:20 and country:cuba
+#input must look like this below
+#country:Jamaica +port:8080
+# query = "country:jamaica +port:8080"
+# encoded_query = urllib.parse.quote_plus(query)
+# print(encoded_query)
+# url = "https://api.zoomeye.org/host/search?query=" + encoded_query + "&page=1"
 print(url)
 response = requests.get(url, headers={"Authorization": "JWT " + token})
 print("\n")
 json_data = response.json()
+#print(json_data)
 length = len(json_data["matches"])
 print(length)
 for i in range(length):
